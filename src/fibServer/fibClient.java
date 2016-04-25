90fbe09b18c61b67e017911ce954b47f0a05b52d
Benj_S <bschurian4@gmail.com>
2016-04-25 12:26:51 +0200

    server fehler werfen

--- src/fibServer/fibClient.java
+++ src/fibServer/fibClient.java
@@ -9,7 +9,13 @@ public class fibClient {
 		final String instruction = "--- Enter a number to fibonaccionize: ";
         System.out.println(instruction);
         try {
-			final Socket client = new Socket(InetAddress.getLocalHost(), 1432);
+			final Socket client ;
+			InetAddress address = InetAddress.getByAddress(args[0].getBytes());
+			int port = -1;
+			port = Integer.parseInt(args[1]);
+			if(address != null)address = InetAddress.getLocalHost();
+			if(port==-1)port = 5678;
+			client = new Socket(address, 5678);
             final PrintWriter out = new PrintWriter(client.getOutputStream(), true);
             final BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
             final BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
@@ -21,7 +27,7 @@ public class fibClient {
                     out.println(number);
                     System.out.println(number + ". number in Fibonacci's sequence:\t\t" + in.readLine());
                     System.out.println(instruction);
-                } catch(Exception e){
+                } catch(IOException e){
                     System.err.println("Please enter a valid number: ");
                 }
             }
