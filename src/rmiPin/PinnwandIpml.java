03dfed19d8d62aab78b644055980adb04b15e0eb
robert_dimitrov <robert.dimitrov3@gmail.com>
2016-05-09 13:26:36 +0200

    cull

--- src/rmiPin/PinnwandIpml.java
+++ src/rmiPin/PinnwandIpml.java
@@ -2,11 +2,12 @@ package rmiPin;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class PinnwandIpml implements Pinnwand {
 	
-	final List<String> messages;
+	final List<Message> messages;
 	final String PASSWORD = "password1234";
 	final int maxNumMessages;
 	/*
@@ -20,7 +21,7 @@ public class PinnwandIpml implements Pinnwand {
 	public PinnwandIpml(int maxNumMessages,
 			long messageLifetime, int maxLengthMessage, String nameOfService) {
 		super();
-		this.messages = new ArrayList<String>();
+		this.messages = new ArrayList<Message>();
 		
 		this.maxNumMessages = maxNumMessages;
 		this.messageLifetime = messageLifetime;
@@ -36,25 +37,60 @@ public class PinnwandIpml implements Pinnwand {
 
 	@Override
 	public int getMessageCount() throws RemoteException {
+		cullExpiredMessages();
 		return messages.size();
 	}
 
 	@Override
 	public String[] getMessages() throws RemoteException {
+		cullExpiredMessages();
 		return (String[]) messages.toArray();
 	}
 
 	@Override
 	public String getMessage(int index) throws RemoteException {
-		return messages.get(index);
+		cullExpiredMessages();
+		return messages.get(index).getMessage();
 	}
 
 	@Override
 	public boolean putMessage(String msg) throws RemoteException {
 		final boolean success;
+		cullExpiredMessages();
 		if(messages.size() >= maxNumMessages) throw new IndexOutOfBoundsException();
 		if(msg.length() > maxLengthMessage) throw new IllegalArgumentException();
-		success = messages.add(msg);
+		success = messages.add(new Message(msg));
 		return success;
 	}
+
+	private void cullExpiredMessages(){
+		long current = System.currentTimeMillis();
+		messages.forEach((message) -> {
+			if(message.getCreationTime() + messageLifetime > current){
+				messages.remove(message);
+			}
+		});
+	}
+
+	private class Message{
+
+		private long creationTime;
+		private String message;
+
+		Message(String message){
+			creationTime = System.currentTimeMillis();
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		public long getCreationTime() {
+			return creationTime;
+		}
+	}
+
 }
+
+
