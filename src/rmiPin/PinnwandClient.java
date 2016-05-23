73325ace4b23e3a0749a79911a575fe92233f963
robert_dimitrov <robert.dimitrov3@gmail.com>
2016-05-23 12:48:50 +0200

    ...

--- src/rmiPin/PinnwandClient.java
+++ src/rmiPin/PinnwandClient.java
@@ -1,23 +1,40 @@
 package rmiPin;
 
-import java.rmi.RemoteException;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 
 public class PinnwandClient {
 
+	public static String serviceName = "pinnwand";
+
 	public static void main(String[] args) {
 
+		Registry registry;
+
 		Pinnwand pinnwand;
+
 		try{
-			pinnwand = 	(PinnwandIpml) Naming.lookup("rmi://localhost/pinnwand");
+			registry = LocateRegistry.getRegistry("localhost");
+
+			pinnwand = 	(PinnwandImpl) registry.lookup(serviceName);
 
 			pinnwand.putMessage("My first message");
 			pinnwand.putMessage("My second message!!");
 			System.out.println(pinnwand.getMessageCount());
 			System.out.println(pinnwand.getMessage(1));
-		} catch(MalformedURLException | RemoteException | NotBoundException e){
+		} catch(RemoteException | NotBoundException e){
+			e.printStackTrace();
+		}
+
+
+		try{
+			pinnwand = (PinnwandImpl) Naming.lookup("rmi://localhost/pinnwand");
+		} catch(RemoteException | NotBoundException | MalformedURLException e){
 			e.printStackTrace();
 		}
 
