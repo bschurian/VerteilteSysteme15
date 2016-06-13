0aeac48300195abb7526446bf6034596e6c44736
Benj_S <bschurian4@gmail.com>
2016-06-13 21:50:50 +0200

    nonstatic

---
+++ src/mailbox/StatusCodes.java
@@ -0,0 +1,12 @@
+package mailbox;
+
+public class StatusCodes {
+	public static final int OKMITANTWORT = 200;
+	public static final int OKOHNEANTWORT = 204;
+	public static final int FEHLRHAFTEANFRAGE = 400;
+	public static final int NICHTEINGELOGGT = 401;
+	public static final int RESOURCENICHTGEFUNDEN = 404;
+	public static final int TOOMANYREQUESTS = 429;
+	public static final int NOTIMPLEMENTED = 501;
+	public static final int SERVICEUNAVAILABLE = 503;
+}
